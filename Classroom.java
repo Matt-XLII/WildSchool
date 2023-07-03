9eb99d09da04f8380f87abd17da9cd0b34c58de0
Matt-XLII <durouchet.mathieu@gmail.com>
2023-07-03 14:57:26 +0200

    updating answer to be more accurate

--- Classroom.java
+++ Classroom.java
@@ -16,43 +16,5 @@ System.out.println(Bryce.whoAmI());
 
 }
 
-class Wilder{
-    private String firstname;
-    private boolean aware;
 
-    public Wilder(String firstname, boolean aware){
-        this.firstname = firstname;
-        this.aware = aware;
-    }
-
-    public String getFirstname(){
-        return this.firstname;
-    }
-
-    public boolean isAware(){
-        return this.aware;
-    }
-
-    public void setFirstname(String firstname){
-        this.firstname = firstname;
-    }
-
-    public void setAware(boolean aware){
-        this.aware = aware;
-        if (aware == true){
-        System.out.println(this.getFirstname() + " est aware");
-        } else {
-            System.out.println(this.getFirstname() + " n'est pas aware");
-    }}
-
-    public String whoAmI(){
-        if (this.aware == true){
-            return "Je m'appelle " + this.firstname + " et je suis aware";
-        } else {
-            return "Je m'appelle " + this.firstname + " et je ne suis pas aware";
-        }
-    }
-
-    
-}
 
